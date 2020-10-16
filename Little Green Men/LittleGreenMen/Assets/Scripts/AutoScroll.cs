97aaf737f7c801b5609094d68a5e73d360a7e7cf
jdelacruz123 <45299615+jdelacruz123@users.noreply.github.com>
2020-10-15 21:32:39 -0400

    First Project Commit
    Contains set up files for this unity project

---
+++ Little Green Men/LittleGreenMen/Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class AutoScroll : MonoBehaviour
+{
+    public float ScrollSpeed; //adjust this in the inspector to make the scroll speed faster or slower
+    public Renderer BackgroundRend; //This is the background renderer used to autoscroll image
+    void Update()
+    {
+        //This is used on a quad and is essentially a moviing sphere with an image cast on it
+        BackgroundRend.material.mainTextureOffset += new Vector2(ScrollSpeed * Time.deltaTime, 0f);
+        
+    }
+}
