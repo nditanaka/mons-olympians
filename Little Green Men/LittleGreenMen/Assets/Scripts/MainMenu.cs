05a736c0d7f710dffcb604cfa75fb686c4c0ca06
jdelacruz123 <45299615+jdelacruz123@users.noreply.github.com>
2020-11-04 13:56:44 -0500

    Navigation Update
    Updated screen navigation so that users can move between screens

---
+++ Little Green Men/LittleGreenMen/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GoHome()
+    {
+        SceneManager.LoadScene("HomeMenu");
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Debug.Log("App has been quit");
+        //Application.Quit;
+    }
+}
