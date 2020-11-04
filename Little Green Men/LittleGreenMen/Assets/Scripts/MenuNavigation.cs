e6c7b0de267ed5b0631380fe0f28de65ee817c85
jdelacruz123 <45299615+jdelacruz123@users.noreply.github.com>
2020-11-04 15:50:54 -0500

    Added Load Screen
    New screen added

---
+++ Little Green Men/LittleGreenMen/Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public void GoHome()
+    {
+        SceneManager.LoadScene("HomeMenu");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("EventScrene");
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Debug.Log("App has been quit");
+        //Application.Quit;
+    }
+
+    // Start is called before the first frame update
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
