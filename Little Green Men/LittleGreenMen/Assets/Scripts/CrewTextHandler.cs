d8d364c4f3ca1855930fe283795c93de5c3708a7
jdelacruz123 <45299615+jdelacruz123@users.noreply.github.com>
2020-11-13 16:00:45 -0500

    Update ship/crew screens
    Added functionality to ship and crew selection screens

---
+++ Little Green Men/LittleGreenMen/Assets/Scripts/CrewTextHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrewTextHandler : MonoBehaviour
+{
+    public Text BigShipText;
+
+    public void BigShip(string text)
+    {
+        BigShipText.text = text;
+
+    }
+}
